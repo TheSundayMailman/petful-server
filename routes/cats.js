ea1c414578fb896f7b42116eb334f4905b33baff
Albert Sare <thesundaymailman@gmail.com>
2018-08-02 21:15:09 -0700

    added basic cats and dogs database with hard-coded sample data

--- routes/cats.js
+++ routes/cats.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express();
 
+const catDb = require('../db/cat-db.js');
+
 router.get('/', (req, res, next) => {
-  res.json({message: 'cats api reached'});
+  res.json(catDb[0]);
 });
 
 module.exports = router;
--- routes/dogs.js
+++ routes/dogs.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const router = express();
 
+const dogDb = require('../db/dog-db.js');
+
 router.get('/', (req, res, next) => {
-  res.json({message: 'dogs api reached'});
+  res.json(dogDb[0]);
 });
 
 module.exports = router;
